388e535f7189b5651c605e12f9971bb6427fbff3
Jose Garcia <jogarcia809@yahoo.com>
2017-11-03 11:01:39 -0400

    added share button and menu button

--- app/src/main/java/com/c4q/recyclerviewed/CustomViewHolder.java
+++ app/src/main/java/com/c4q/recyclerviewed/CustomViewHolder.java
@@ -1,9 +1,12 @@
 package com.c4q.recyclerviewed;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A view holder is responsible to taking care of what happens in one individual item.
@@ -11,7 +14,9 @@ import android.widget.TextView;
  * binds the new info to the created view-holder, instead of creating a new one, which it only does if it needs to.
  */
 
-class CustomViewHolder extends RecyclerView.ViewHolder {
+class CustomViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    private TextView textView;
 
     //Constructor takes in the view created with our specific layout. This view is passed in from the adapter.
     public CustomViewHolder(View itemView) {
@@ -22,7 +27,31 @@ class CustomViewHolder extends RecyclerView.ViewHolder {
     public void bind(String color, int position) {
         // in our case we are setting a background color based on the list index and setting a textview's text to the index of the list item.
         itemView.setBackgroundColor(Color.parseColor(color));
-        TextView textView = (TextView) itemView.findViewById(R.id.item_position_text_view);
+
+        textView = (TextView) itemView.findViewById(R.id.item_position_text_view);
+        Button button = (Button) itemView.findViewById(R.id.share_button);
         textView.setText(String.valueOf(position));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent(Intent.ACTION_SEND)
+                        .putExtra(Intent.EXTRA_TEXT, textView.getText())
+                        .setType("text/plain");
+                if(sendIntent
+                        .resolveActivity(view.getContext()
+                                .getPackageManager()) != null){
+                    view
+                            .getContext()
+                            .startActivity(sendIntent);
+                }else{
+                    Toast.makeText(view.getContext(), "DOesnt work", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
